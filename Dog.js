ca0bbc10c13b99dbf16ea77dcda38aa824c2e8f0
AnhPhuong <anhphuongdh9a@gmail.com>
2019-02-17 23:14:04 +0700

    Fix: Missing export for Dog and add a dog to index,js file

--- Dog.js
+++ Dog.js
@@ -1,7 +1,10 @@
-function Dog() {
+function Dog(name) {
+	this.name = name;
 	this.stomach = [];
 }
 
 Dog.prototype.eat = function(cat) {
 	this.stomach.push(cat);
 };
+
+module.exports = Dog;
--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 var Cat = require('./Cat.js');
 var Mouse  = require('./Mouse.js');
+var Dog = require('./Dog.js');
 
 var cat = new Cat('Tom');
 var mouse = new Mouse('Jerry');
+var dog = new Dog('Fox');
+
 cat.eat(mouse);
 mouse.dead();
+dog.eat(cat);
+
 console.log(cat);
 console.log(mouse);
+console.log(dog);
